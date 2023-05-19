9df93ed8c34d8a7ad964eed61a2a9dd21899a90f
hasanalt <93833148+hasanalt@users.noreply.github.com>
2023-05-19 10:27:01 +0300

    Update ADB_server.py

--- ADB_server.py
+++ ADB_server.py
@@ -1,66 +1,75 @@
-import os, time
-
+import os
 from ppadb.client import Client as AdbClient
 from loguru import logger
 
 
-class ADB_server:
+class AdbBase:
     def __init__(self, port):
-        # automatically scan for bluetooth's port
+        self.port = self._get_adb_port(port)
+        self.client = self._get_adb_client()
+        self.device = self._get_device()
+
+    def _get_adb_port(self, port):
         if port.lower() == "bluestacks" or port == "":
             with open(r"C:\ProgramData\BlueStacks_nxt\bluestacks.conf", "r") as f:
                 lines = f.readlines()
                 for l in lines:
                     if "bst.instance.Nougat64.status.adb_port=" in l:
-                        port = int(l.split("=")[-1][1:-2])
-                        break
-        logger.debug(f"ADB_server().__init__({port})")
+                        return int(l.split("=")[-1][1:-2])
+        return int(port)
+
+    def _get_adb_client(self):
         os.system(r'"adb\adb kill-server"')
         os.system(r'"adb\adb start-server"')
-        os.system(fr'"adb\adb connect 127.0.0.1:{port}"')
-        client = AdbClient(host="127.0.0.1", port=5037)
-        devices = client.devices()
-        self.device = devices[0]
+        os.system(fr'"adb\adb connect 127.0.0.1:{self.port}"')
+        return AdbClient(host="127.0.0.1", port=5037)
 
-    def cheakInstallCR(self):
-        logger.debug("cheakInstallCR")
+    def _get_device(self):
+        devices = self.client.devices()
+        return devices[0]
+
+    def check_install_cr(self):
+        logger.debug("check_install_cr")
         packages = self.device.shell("pm list packages").split("\r\n")[0].split("\n")
         for package in packages:
             if package == "package:com.supercell.clashroyale":
-                logger.debug("cheakInstallCR -> True")
+                logger.debug("check_install_cr -> True")
                 return True
-        logger.debug("cheakInstallCR -> False")
+        logger.debug("check_install_cr -> False")
         return False
 
-    def cheakRunCR(self):
-        logger.debug("cheakRunCR")
+    def check_run_cr(self):
+        logger.debug("check_run_cr")
         if self.device.get_pid("com.supercell.clashroyale"):
-            logger.debug("cheakRunCR -> True")
+            logger.debug("check_run_cr -> True")
             return True
-        logger.debug("cheakRunCR -> False")
+        logger.debug("check_run_cr -> False")
         return False
 
-    def openCR(self):
-        logger.debug("openCR")
+    def open_cr(self):
+        logger.debug("open_cr")
         self.device.shell("monkey -p com.supercell.clashroyale -v 15")
 
-    def closeCR(self):
-        logger.debug("closeCR")
+    def close_cr(self):
+        logger.debug("close_cr")
         self.device.shell("am force-stop com.supercell.clashroyale")
 
     def click(self, x: int, y: int):
-        logger.debug(f"ADB_server.click x = {x} y = {y}")
+        logger.debug(f"click x = {x} y = {y}")
         self.device.shell(f"input tap {str(x)} {(str(y))}")
 
     def swipe(self, x1: int, y1: int, x2: int, y2: int):
-        logger.debug(f"ADB_server.swipe x1 = {x1} y1 = {y1} x2 = {x2} y2 = {y2}")
-        self.device.shell(
-            f"input swipe {str(x1)} {(str(y1))} {str(x2)} {(str(y2))} 2000"
-        )
+        logger.debug(f"swipe x1 = {x1} y1 = {y1} x2 = {x2} y2 = {y2}")
+        self.device.shell(f"input swipe {str(x1)} {(str(y1))} {str(x2)} {(str(y2))} 2000")
 
-    def getScreen(self):
-        logger.debug("ADB_server.getScreen")
+    def get_screen(self):
+        logger.debug("get_screen")
         return self.device.screencap()
+
+
+class AdbServer(AdbBase):
+    def __init__(self, port):
+        super().__init__(port)
 
     def add_client(self, port):
         os.system(fr'"adb\adb connect 127.0.0.1:{port}"')
@@ -72,49 +81,9 @@ class ADB_server:
         self.device.shell("reboot")
 
 
-class ADB_client:
+class AdbClient(AdbBase):
     def __init__(self, port: int):
-        self.device = self.add_client(port)
-
-    def cheakInstallCR(self):
-        logger.debug("cheakInstallCR")
-        packages = self.device.shell("pm list packages").split("\r\n")[0].split("\n")
-        for package in packages:
-            if package == "package:com.supercell.clashroyale":
-                logger.debug("cheakInstallCR -> True")
-                return True
-        logger.debug("cheakInstallCR -> False")
-        return False
-
-    def cheakRunCR(self):
-        logger.debug("cheakRunCR")
-        if self.device.get_pid("com.supercell.clashroyale"):
-            logger.debug("cheakRunCR -> True")
-            return True
-        logger.debug("cheakRunCR -> False")
-        return False
-
-    def openCR(self):
-        logger.debug("openCR")
-        self.device.shell("monkey -p com.supercell.clashroyale -v 15")
-
-    def closeCR(self):
-        logger.debug("closeCR")
-        self.device.shell("am force-stop com.supercell.clashroyale")
-
-    def click(self, x: int, y: int):
-        logger.debug(f"ADB_server.click x = {x} y = {y}")
-        self.device.shell(f"input tap {str(x)} {(str(y))}")
-
-    def swipe(self, x1: int, y1: int, x2: int, y2: int):
-        logger.debug(f"ADB_server.swipe x1 = {x1} y1 = {y1} x2 = {x2} y2 = {y2}")
-        self.device.shell(
-            f"input swipe {str(x1)} {(str(y1))} {str(x2)} {(str(y2))} 2000"
-        )
-
-    def getScreen(self):
-        logger.debug("ADB_server.getScreen")
-        return self.device.screencap()
+        super().__init__(port)
 
     def add_client(self, port):
         pass
